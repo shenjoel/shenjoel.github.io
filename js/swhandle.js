c6c20fd69297b222c03f0541e325b6a39390a2a9
joel.shen <joel.shen@vipshop.com>
2017-02-17 18:59:49 +0800

    deployment

--- js/swhandle.js
+++ js/swhandle.js
@@ -1,6 +1,6 @@
 window.addEventListener('beforeinstallprompt', function(e) {
     // beforeinstallprompt Event fired
-    alert('show add home screen');
+    // alert('show add home screen');
     // e.userChoice will return a Promise. 
     // For more details read: https://developers.google.com/web/fundamentals/getting-started/primers/promises
     e.userChoice.then(function(choiceResult) {
