554222b6c52be8040fff7768fe80ee4ccace0042
joel.shen <joel.shen@vipshop.com>
2017-02-20 14:16:25 +0800

    deployment

--- sw.js
+++ sw.js
@@ -12,39 +12,39 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // 
-var cacheName = 'joelblog-sorce-v1.0.1';
-var filesToCache = [
-    '/',
-    '/index.html',
-    '/css/style.css',
-    '/js/main.js',
-    '/js/search.js',
-    '/img/brand.png',
-    '/img/back.png'
-];
+// var cacheName = 'joelblog-sorce-v1.0.1';
+// var filesToCache = [
+//     '/',
+//     '/index.html',
+//     '/css/style.css',
+//     '/js/main.js',
+//     '/js/search.js',
+//     '/img/brand.png',
+//     '/img/back.png'
+// ];
 
 self.addEventListener('install', function(e) {
     console.log('[ServiceWorker] Install');
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            console.log('[ServiceWorker] Caching app shell');
-            return cache.addAll(filesToCache);
-        })
-    );
+    // e.waitUntil(
+    //     caches.open(cacheName).then(function(cache) {
+    //         console.log('[ServiceWorker] Caching app shell');
+    //         return cache.addAll(filesToCache);
+    //     })
+    // );
 });
 
 self.addEventListener('activate', function(e) {
     console.log('[ServiceWorker] Activate');
-    e.waitUntil(
-        caches.keys().then(function(keyList) {
-            return Promise.all(keyList.map(function(key) {
-                if (key !== cacheName) {
-                    console.log('[ServiceWorker] Removing old cache', key);
-                    return caches.delete(key);
-                }
-            }));
-        })
-    );
+    // e.waitUntil(
+    //     caches.keys().then(function(keyList) {
+    //         return Promise.all(keyList.map(function(key) {
+    //             if (key !== cacheName) {
+    //                 console.log('[ServiceWorker] Removing old cache', key);
+    //                 return caches.delete(key);
+    //             }
+    //         }));
+    //     })
+    // );
     /*
      * Fixes a corner case in which the app wasn't returning the latest data.
      * You can reproduce the corner case by commenting out the line below and
@@ -55,22 +55,22 @@ self.addEventListener('activate', function(e) {
      * service worker is not yet activated. The code below essentially lets
      * you activate the service worker faster.
      */
-    return self.clients.claim();
+    // return self.clients.claim();
 });
 
 self.addEventListener('fetch', function(e) {
     console.log('[Service Worker] Fetch', e.request.url);
-    var dataUrl = 'https://shenjoel.github.io/';
-    if (e.request.url.indexOf(dataUrl) > -1) {
-        /*
-         * The app is asking for app shell files. In this scenario the app uses the
-         * "Cache, falling back to the network" offline strategy:
-         * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
-         */
-        e.respondWith(
-            caches.match(e.request).then(function(response) {
-                return response || fetch(e.request);
-            })
-        );
-    }
+    // var dataUrl = 'https://shenjoel.github.io/';
+    // if (e.request.url.indexOf(dataUrl) > -1) {
+        
+    //      // * The app is asking for app shell files. In this scenario the app uses the
+    //      // * "Cache, falling back to the network" offline strategy:
+    //      // * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+         
+    //     e.respondWith(
+    //         caches.match(e.request).then(function(response) {
+    //             return response || fetch(e.request);
+    //         })
+    //     );
+    // }
 });
