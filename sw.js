44d7afbe8dd151d71c2527dd7d5af416df0d97dc
joel.shen <joel.shen@vipshop.com>
2017-02-20 14:46:28 +0800

    deployment

--- sw.js
+++ sw.js
@@ -23,54 +23,54 @@
 //     '/img/back.png'
 // ];
 
-self.addEventListener('install', function(e) {
-    console.log('[ServiceWorker] Install');
-    // e.waitUntil(
-    //     caches.open(cacheName).then(function(cache) {
-    //         console.log('[ServiceWorker] Caching app shell');
-    //         return cache.addAll(filesToCache);
-    //     })
-    // );
-});
+// self.addEventListener('install', function(e) {
+//     console.log('[ServiceWorker] Install');
+//     e.waitUntil(
+//         caches.open(cacheName).then(function(cache) {
+//             console.log('[ServiceWorker] Caching app shell');
+//             return cache.addAll(filesToCache);
+//         })
+//     );
+// });
 
-self.addEventListener('activate', function(e) {
-    console.log('[ServiceWorker] Activate');
-    // e.waitUntil(
-    //     caches.keys().then(function(keyList) {
-    //         return Promise.all(keyList.map(function(key) {
-    //             if (key !== cacheName) {
-    //                 console.log('[ServiceWorker] Removing old cache', key);
-    //                 return caches.delete(key);
-    //             }
-    //         }));
-    //     })
-    // );
-    /*
-     * Fixes a corner case in which the app wasn't returning the latest data.
-     * You can reproduce the corner case by commenting out the line below and
-     * then doing the following steps: 1) load app for first time so that the
-     * initial New York City data is shown 2) press the refresh button on the
-     * app 3) go offline 4) reload the app. You expect to see the newer NYC
-     * data, but you actually see the initial data. This happens because the
-     * service worker is not yet activated. The code below essentially lets
-     * you activate the service worker faster.
-     */
-    // return self.clients.claim();
-});
+// self.addEventListener('activate', function(e) {
+//     console.log('[ServiceWorker] Activate');
+//     e.waitUntil(
+//         caches.keys().then(function(keyList) {
+//             return Promise.all(keyList.map(function(key) {
+//                 if (key !== cacheName) {
+//                     console.log('[ServiceWorker] Removing old cache', key);
+//                     return caches.delete(key);
+//                 }
+//             }));
+//         })
+//     );
+    
+//      * Fixes a corner case in which the app wasn't returning the latest data.
+//      * You can reproduce the corner case by commenting out the line below and
+//      * then doing the following steps: 1) load app for first time so that the
+//      * initial New York City data is shown 2) press the refresh button on the
+//      * app 3) go offline 4) reload the app. You expect to see the newer NYC
+//      * data, but you actually see the initial data. This happens because the
+//      * service worker is not yet activated. The code below essentially lets
+//      * you activate the service worker faster.
+     
+//     return self.clients.claim();
+// });
 
-self.addEventListener('fetch', function(e) {
-    console.log('[Service Worker] Fetch', e.request.url);
-    // var dataUrl = 'https://shenjoel.github.io/';
-    // if (e.request.url.indexOf(dataUrl) > -1) {
-        
-    //      // * The app is asking for app shell files. In this scenario the app uses the
-    //      // * "Cache, falling back to the network" offline strategy:
-    //      // * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
-         
-    //     e.respondWith(
-    //         caches.match(e.request).then(function(response) {
-    //             return response || fetch(e.request);
-    //         })
-    //     );
-    // }
-});
+// self.addEventListener('fetch', function(e) {
+//     console.log('[Service Worker] Fetch', e.request.url);
+//     var dataUrl = 'https://shenjoel.github.io/';
+//     if (e.request.url.indexOf(dataUrl) > -1) {
+//         /*
+//          * The app is asking for app shell files. In this scenario the app uses the
+//          * "Cache, falling back to the network" offline strategy:
+//          * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+//          */
+//         e.respondWith(
+//             caches.match(e.request).then(function(response) {
+//                 return response || fetch(e.request);
+//             })
+//         );
+//     }
+// });
